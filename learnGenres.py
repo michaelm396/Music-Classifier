3f091d477a4daa814cfaf25ea9e4620ae27ea55c
ecompton <ecompton@andrew.cmu.edu>
2018-11-23 18:42:11 -0500

    added song usage

--- learnGenres.py
+++ learnGenres.py
@@ -4,15 +4,23 @@ import os
 
 def main():
     #go through songs in data set and construct train and test lists
-    #train = [[0,1,1,"A"],[0,2,1,"A"],[5,5,1,"B"],[4,6,1,"B"],[1,2,1,"B"],[4,4,1,"A"]]
-    #test = [[0,0,1,"A"],[7,7,1,"B"],[3,3,1,"A"]]
+
     train = []
     test = []
-    song = 'Songs/Single-Ladies.mp3'
-    L = songToFeatures(song,"pop")
-    train += [L]
-    test += [L]
-    #run knn and output results
+    genreList = os.listdir("Songs")
+    for g in genreList:
+        if (g != ".DS_Store"):
+            genreSongs = os.listdir("Songs/"+g)
+            for song in genreSongs:
+                if (song != ".DS_Store"):
+                    try:
+                        L = songToFeatures("Songs/"+g+"/"+song,g)
+                        test += [L]
+                        train += [L]
+                    except:
+                        continue
+    print(test)
+
     print(knn(train,test,3))
 
 if __name__ == "__main__":
--- parseAudioFile.py
+++ parseAudioFile.py
@@ -5,16 +5,12 @@ import os
 #from pydub.playback import play #pip install pyaudio
 
 def songToFeatures(filePath,genre):
-    #song = '/Songs/Single-Ladies.mp3' 
-    #path = os.path.dirname(os.getcwd()) + song
-    #song = songToFeatures(path,"pop")
-    #sound = AudioSegment.from_mp3(filePath)
+
     Features = Genre(filePath)
     maxAmplitude = Features.max_amplitude()
     loudness = Features.loud()
     duration_len = Features.duration()
-    frame_rate = Features.frame_rate()
-    sample_size = Features.sample_size()
-    resArr = [maxAmplitude,loudness,duration_len,frame_rate,sample_size,genre]
-    print resArr
+    
+    resArr = [maxAmplitude,loudness,duration_len,genre]
+
     return resArr
