6468ec90cccf363b06be6edd9cd06adad2d11558
ecompton <ecompton@andrew.cmu.edu>
2018-12-02 17:04:08 -0500

    added songs to rap,pop,country

--- parseAudioFile.py
+++ parseAudioFile.py
@@ -8,17 +8,20 @@ def songToFeatures(filePath,genre):
 
     Features = Genre(filePath)
     #maxAmplitude = Features.max_amplitude()
-    loudness = (Features.loud() / 16332.0)*3
+    loudness = (Features.loud() / 16332.0)*4
     duration_len = (Features.duration() / 374.19)
     avgChange = (Features.parseBySecond() / 4624.60137)
     avgAmpChange = (Features.amplitudePerMili() / 4751.36598)
     BPM = (Features.beats_per_minute() / 1301.76) #divide by average
 
-    print(genre)
-    print(BPM)
-    print("\n")
+    #print(genre)
+    #print(BPM)
+    #print("\n")
 
     resArr = [avgAmpChange,loudness,duration_len,avgChange,BPM,genre]
+    print(filePath)
+    #resArr = [BPM,genre]
+    #print (BPM,genre)
     #resArr = [avgAmpChange,genre]
     #print(resArr+[filePath])
     #print(avgAmpChange)
