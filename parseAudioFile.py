b47cab426b1919940787e32f21fbafb568de7b43
ecompton <ecompton@andrew.cmu.edu>
2018-12-07 00:43:38 -0500

    updated song files

--- parseAudioFile.py
+++ parseAudioFile.py
@@ -12,22 +12,22 @@ def songToFeatures(filePath,genre):
     duration_len = (Features.duration() / 374.19)
     avgChange = (Features.parseBySecond() / 4624.60137)
     avgAmpChange = (Features.amplitudePerMili() / 4751.36598)
-    Energy = (Features.energy() / 55.81395349) #divide by average
+    #Energy = (Features.energy() / 55.81395349) #divide by average
     Freq = (Features.frequency_fft()/ 202.5245728)
 
-    if ("Songs/train" in filePath):
+    """if ("Songs/train" in filePath):
         myFile = open('csvtrain.csv', 'a')  
         with myFile as f: 
             writer = csv.writer(f)
-            writer.writerow([avgAmpChange,loudness,duration_len,avgChange,Energy,Freq])
+            writer.writerow([avgAmpChange,loudness,duration_len,avgChange,Energy, Freq])
     
     if ("Songs/test" in filePath):
         myFile = open('csvtest.csv', 'a')   
         with myFile as f:  
             writer = csv.writer(f)
-            writer.writerow([avgAmpChange,loudness,duration_len,avgChange,Energy,Freq])
+            writer.writerow([avgAmpChange,loudness,duration_len,avgChange,Energy,Freq])"""
 
-    resArr = [avgAmpChange,loudness,duration_len,avgChange,Energy,Freq,genre]
+    resArr = [avgAmpChange,loudness,duration_len,avgChange,Freq,genre]
     #resArr = [avgAmpChange,loudness,duration_len,avgChange,genre] #returns coorectness of 74%
     #resArr = [avgAmpChange,loudness,duration_len,avgChange,genre] # returns correctness of 68%
     #resArr = [loudness,genre]
