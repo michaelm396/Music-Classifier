89cc4a424a4b9a15fe923e685688381919a527e1
Michael Moorer Jr <michael.moorer396@gmail.com>
2019-05-07 20:58:03 -0400

    Learn genre knn classifer
    This is the knn classifier file which seperates files.

---
+++ learnGenres-Knn.py
@@ -0,0 +1,43 @@
+from simpleKnnClassifier import knn
+from parseAudioFile import songToFeatures
+import os
+from sklearn.svm import SVC
+import pdb
+
+def main():
+    #go through songs in data set and construct train and test lists
+
+    train = []
+    test = []
+
+    trainGenreList = os.listdir("Songs/train")
+    for g in trainGenreList:
+        if (g != ".DS_Store"):
+            genreSongs = os.listdir("Songs/train/"+g)
+            for song in genreSongs:
+                if (song != ".DS_Store"):
+                    try:
+                        L = songToFeatures("Songs/train/"+g+"/"+song,g)
+                        train += [L]
+                    except:
+                        continue
+
+    testGenreList = os.listdir("Songs/test") 
+    for g in testGenreList:
+        if (g != ".DS_Store"):
+            genreSongs = os.listdir("Songs/test/"+g)
+            for song in genreSongs:
+                if (song != ".DS_Store"):
+                    try:
+                        L = songToFeatures("Songs/test/"+g+"/"+song,g)
+                        test += [L]
+                    except:
+                        continue
+
+    print(test)
+    #print(songToFeatures("Songs/pop/Single-Ladies.mp3","pop"))
+
+    print(knn(train,test,6))
+
+if __name__ == "__main__":
+    main()
