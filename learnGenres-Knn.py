e890f2d3cf624539bf874a3d66190275def51f76
Michael Moorer Jr <michael.moorer396@gmail.com>
2019-05-08 21:49:20 -0400

    update KNN

--- learnGenres-Knn.py
+++ learnGenres-Knn.py
@@ -1,50 +1,38 @@
-from knnCuda import knn
+from simpleKnnClassifier import knn
 from parseAudioFile import songToFeatures
 import os
+from sklearn.svm import SVC
 import pdb
+import logging
+import csv
 
 def main():
     #go through songs in data set and construct train and test lists
 
     train = []
     test = []
-    
+    #trainGenreList = os.listdir("Songs/train")
     TEST = open("TEST_DATA.csv")
     reader = csv.reader(TEST)
     test = list(reader)
-    
+ 
+    #testGenreList = os.listdir("Songs/test") 
     TRAIN = open("TRAINING_DATA.csv")
     reader2 = csv.reader(TRAIN)
     train = list(reader2)
-    """
-    trainGenreList = os.listdir("Songs/train")
-    for g in trainGenreList:
-        if (g != ".DS_Store"):
-            genreSongs = os.listdir("Songs/train/"+g)
-            for song in genreSongs:
-                if (song != ".DS_Store"):
-                    try:
-                        L = songToFeatures("Songs/train/"+g+"/"+song,g)
-                        train += [L]
-                    except:
-                        continue
-
-    testGenreList = os.listdir("Songs/test") 
-    for g in testGenreList:
-        if (g != ".DS_Store"):
-            genreSongs = os.listdir("Songs/test/"+g)
-            for song in genreSongs:
-                if (song != ".DS_Store"):
-                    try:
-                        L = songToFeatures("Songs/test/"+g+"/"+song,g)
-                        test += [L]
-                    except:
-                        continue
-    """
-    print(test)
+    
+    #pdb.set_trace()
+    #print(test)
     #print(songToFeatures("Songs/pop/Single-Ladies.mp3","pop"))
 
     print(knn(train,test,6))
 
 if __name__ == "__main__":
     main()
+    #import timeit
+    #setup = "from __main__ import main2"
+    #LOG_FILE_NAME = "logfile_genre_classifyer2.log"
+    #logging.basicConfig(filename = LOG_FILE_NAME,filemode = 'w+',level = logging.DEBUG,format = '%(asctime)s - %(levelname)s: %(message)s',\
+                     #datefmt = '%m/%d/%Y %I:%M:%S %p')
+    #logging.debug(timeit.timeit("main2()", setup=setup))
+    #logging.debug("COMPLETE")
